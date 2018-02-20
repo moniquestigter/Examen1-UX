df5e78b50f802730b666f9c0fcc01bc0f3126274
moniquestigter <mstigter17@hotmail.com>
2018-02-19 19:01:29 -0600

    locations medio hecho

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,14 +3,46 @@ class LocationsController < ApplicationController
 	def index
 		@locations = Location.all.order('Created_at DESC')
 	end
-	def new
 
+	def new
+		@location = Location.new
+	end
+
+	def edit
+		@location = Location.find(params[:id])
 	end
 
 	def create
+		@location = current_user.locations.new(location_params)
+
+		@location.save
+			redirect_to @location
 	end 
 
 	def show
+		@location = Location.find(params[:id])
 	end
+
+	def update
+		@location = Location.find(params[:id])
+
+		if @location.update(post_params)
+			redirect_to @location
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@location = Location.find(params[:id])
+		@location.destroy
+
+		redirect_to locations_path
+	end
+
+	private
+		def location_params
+			params.require(:location).permit(:name)
+		end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :locations do
+  	resources :recordings
+  end
   
   root 'locations#index'
+
 end
