4d2098cdd2cb4512659bea5d077cd0019ed9c351
moniquestigter <mstigter17@hotmail.com>
2018-02-19 18:11:47 -0600

    creadas las tablas

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,8 +1,16 @@
 class LocationsController < ApplicationController
+	before_action :authenticate_user!
+	def index
+		@locations = Location.all.order('Created_at DESC')
+	end
 	def new
+
 	end
 
 	def create
 	end 
+
+	def show
+	end
 
 end
